9f2477f33ac5ba08062c0bbd585b42694dda44d7
Edvard <edvardlindelof@gmail.com>
2016-11-16 15:30:34 +0100

    how-to define edges in geometries

--- geometries.py
+++ geometries.py
@@ -5,9 +5,9 @@ HOLE_DIST = 0.2
 
 def box_with_hole():
     return _edges_to_points([
-        [0, 10, 0, 0],
-        [0, 0, 10, 0],
-        [10, 0, 10, 10],
+        [0, 10, 0, 0],  # define edges like so: [x_a, y_a, x_b, y_b]
+        [0, 0, 10, 0],  # (x_a, y_a) is the starting point of the edge
+        [10, 0, 10, 10],  # (x_b, y_b) is the end point
         [0, 10, 4, 10],
         [6, 10, 10, 10]
     ])
@@ -34,4 +34,4 @@ def _edges_to_points(edges):
         pt_b = np.array([edges[i][2], edges[i][3]]).reshape(1, 2)
         new_pts = dotted_edge(pt_a, pt_b)
         pts = np.vstack([pts, new_pts])
-    return pts[1:,:]
+    return pts[1:, :]
